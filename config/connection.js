6e91134d4dcc431c08750cf18f258b6641bfe207
Chice <cko517@hotmail.com>
2022-01-08 02:46:52 -0500

    Create PUT Route for Voting on a Post

--- config/connection.js
+++ config/connection.js
@@ -4,10 +4,15 @@ const Sequelize = require('sequelize');
 require('dotenv').config();
 
 // creates connection to the database, passes in MySQL info, username and password
-const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
-  host: 'localhost',
-  dialect: 'mysql',
-  port: 3306
-});
+const sequelize = new Sequelize(
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PW,
+  {
+    host: 'localhost',
+    dialect: 'mysql',
+    port: 3306
+  }
+);
 
 module.exports = sequelize;
