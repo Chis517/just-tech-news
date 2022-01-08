38a07e3dbd871e9b6da290fbc4f0e633ecc298be
Chice <cko517@hotmail.com>
2022-01-08 02:58:21 -0500

    Update GET Routes to include Votes

--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -25,6 +25,12 @@ router.get('/:id', (req, res) => {
       {
         model: Post,
         attributes: ['id', 'title', 'post_url', 'created_at']
+      },
+      {
+        model: Post,
+        attributes: ['title'],
+        through: Vote,
+        as: 'voted_posts'
       }
     ]
   })
