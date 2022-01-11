21d615b431a8290a94771638a42dcd2475448476
Chice <cko517@hotmail.com>
2022-01-10 20:45:40 -0500

    Create a session on the back end

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -3,6 +3,7 @@ const sequelize = require('../config/connection');
 const { Post, User, Comment, Vote } = require('../models');
 
 router.get('/', (req, res) => {
+  console.log(req.session);
   Post.findAll({
     attributes: [
       'id',
@@ -46,7 +47,12 @@ router.get('/', (req, res) => {
 });
 
 router.get('/login', (req, res) => {
+  if (req.session.loggedIn) {
+    res.redirect('/');
+    return;
+  }
+
   res.render('login');
-})
+});
 
 module.exports = router;
